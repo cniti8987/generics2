d3ffd4915fe5e7ca5deeb35450c1cb92b46472f9
niti <cniti8990@gmail.com>
2020-08-17 16:09:22 -0400

    govinda

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mygenericlist<string> strlist = new Mygenericlist<string>();
+            strlist.add("rsdhakrisna");
+
+            Mygenericlist<int> intlist = new Mygenericlist<int>();
+            intlist.add(1);
+
+
+            Console.WriteLine(strlist.Get());
+            Console.WriteLine(intlist.Get());
+            Mygenericlist<customer> cuslist = new Mygenericlist<customer>();
+
+        }
+    }
+    public class customer
+    {
+        public string Name { get; set; }
+    }
+    public class Mygenericlist<T>
+    {
+        private T[] _stringarray;
+        private int _addposition = 0;
+        private int _getposition = 0;
+
+        public Mygenericlist()
+        {
+            _stringarray = new T[1000];
+
+        }
+
+        public void add(T item)
+        {
+            _stringarray[_addposition] = item;
+            _addposition++;
+        }
+        public T Get()
+        {
+            T item = _stringarray[_getposition];
+            _getposition++;
+            return item;
+        }
+    }
+}
